9daf7d9abe6831db8946ce375f6b2b9d88c5aed8
Cristian Vera <veracristian89@gmail.com>
2023-02-09 12:22:59 -0300

    se deja funcionando dto

--- src/main/java/com/login/backendlogin/Controller/LoginController.java
+++ src/main/java/com/login/backendlogin/Controller/LoginController.java
@@ -7,6 +7,9 @@ package com.login.backendlogin.Controller;
 import com.login.backendlogin.dto.AccountDto;
 import com.login.backendlogin.model.Account;
 import com.login.backendlogin.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -18,11 +21,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoginController {
     
-    User usuario = new User(1,"Cristian","Vera","Av Evergreen 555");
-    Account cuenta = new Account(1,1,"null","null");
+    @GetMapping("/usuario/{id}")
+        @ResponseBody
+        public AccountDto devolverCuenta(@PathVariable long id){
+            User usuario = new User(10,"Cristian","Vera","Av Evergreen 555");
+            Account cuenta = new Account(10,10,"veracristian89","clave123");
     
-    AccountDto cuentaDto = new AccountDto();
+            AccountDto cuentaDto = new AccountDto();
     
-    cuentaDto.setAcc_id(cuenta.getAcc_id());
-    
+            cuentaDto.setAcc_id(cuenta.getAcc_id());
+            cuentaDto.setAcc_usr_id(cuenta.getAcc_usr_id());
+            cuentaDto.setAcc_username(cuenta.getAcc_username());
+            cuentaDto.setAcc_password(cuenta.getAcc_password());
+            cuentaDto.setUsr_id(usuario.getUsr_id());
+            cuentaDto.setUsr_first_name(usuario.getUsr_first_name());
+            cuentaDto.setUsr_last_name(usuario.getUsr_last_name());
+            cuentaDto.setUsr_address(usuario.getUsr_address());
+            
+            return cuentaDto;
+        }
 }
